72bf72fa3a551de11ef65cfaa7315fbda59c01fb
Jonas <jonas.klugmann@gmail.com>
2020-05-11 21:38:02 +0200

    init

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "MV Buddies Status",
+  "name": "mv_buddies_status",
+  "description": "See the status of the MV Buddies Services",
+  "defaultLocale": "de",
+  "locales": [
+    {
+      "code": "de",
+      "iso": "de-DE",
+      "name": "Deutsch",
+      "file": "de.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
